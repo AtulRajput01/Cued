1f17165ce78cab95051998c257fa65bed58eb52b
Atul Rajput <92659293+AtulRajput01@users.noreply.github.com>
2023-09-27 15:01:37 +0530

    Update Login.js

--- razecued/src/screens/Login.js
+++ razecued/src/screens/Login.js
@@ -1,47 +1,49 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, Animated, TouchableOpacity, Image, StyleSheet, Pressable, TextInput, Button, Easing, Keyboard } from 'react-native';
+import { View, Text, Animated, TouchableOpacity, Image, StyleSheet, Pressable, TextInput, Easing, Keyboard } from 'react-native';
 import { ImageBackground } from 'react-native';
-import CheckBox from 'react-native-check-box'
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import CheckBox from 'react-native-check-box';
 import { Auth } from 'aws-amplify';
-import config from '../aws-exports'; // Import Amplify config
 import {Amplify} from 'aws-amplify';
+import config from '../aws-exports'; // Import Amplify config
 Amplify.configure(config); // Configure Amplify
 
-const Login = ({ navigation }) => {
+
+const RegisterAndSignup = ({ navigation }) => {
   const transY = useRef(new Animated.Value(0));
+  const transYN = useRef(new Animated.Value(0));
+  const transYE = useRef(new Animated.Value(0));
   const transYP = useRef(new Animated.Value(0));
+  const transYC = useRef(new Animated.Value(0));
 
   const handleFocus = (animatedValue) => {
     Animated.timing(animatedValue, {
       toValue: -35,
       duration: 300,
       useNativeDriver: true,
-      easing: Easing.ease
+      easing: Easing.ease,
     }).start();
-  }
+  };
 
   const handleBlur = (animatedValue, text) => {
     if (!text) {
       Animated.timing(animatedValue, {
         toValue: 0,
         useNativeDriver: true,
-        easing: Easing.ease
+        easing: Easing.ease,
       }).start();
     }
-  }
+  };
 
-  const handleKeyboardDismiss = () => {
-    Keyboard.dismiss();
-  }
-
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [loginResponse, setLoginResponse] = useState(null);
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false); // Add state for password visibility
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
+  const [confirmationSent, setConfirmationSent] = useState(false);
+  const [registrationResponse, setRegistrationResponse] = useState(null);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
   const togglePasswordVisibility = () => {
     setIsPasswordVisible(!isPasswordVisible);
@@ -52,40 +54,64 @@ const Login = ({ navigation }) => {
   const transX = transY.current.interpolate({
     inputRange: [-35, 0],
     outputRange: [-20, 0],
-    extrapolate: 'clamp'
+    extrapolate: 'clamp',
+  });
+
+  const transXN = transYN.current.interpolate({
+    inputRange: [-35, 0],
+    outputRange: [-20, 0],
+    extrapolate: 'clamp',
+  });
+
+  const transXE = transYE.current.interpolate({
+    inputRange: [-35, 0],
+    outputRange: [-20, 0],
+    extrapolate: 'clamp',
   });
 
   const transXP = transYP.current.interpolate({
     inputRange: [-35, 0],
     outputRange: [-20, 0],
-    extrapolate: 'clamp'
+    extrapolate: 'clamp',
   });
 
-  const handleLogin = async () => {
+  const transXC = transYC.current.interpolate({
+    inputRange: [-35, 0],
+    outputRange: [-20, 0],
+    extrapolate: 'clamp',
+  });
+
+  const handleRegister = async () => {
     setIsLoading(true);
     setError('');
+    setConfirmationSent(false);
 
     try {
-      // Input validation
-      if (!email || !password) {
-        throw new Error('Email and password are required.');
+      // Check for password complexity (e.g., at least 8 characters, 1 uppercase, 1 digit)
+      if (!/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(password)) {
+        throw new Error('Password must be at least 8 characters long and include at least one uppercase letter and one digit.');
       }
+      const signUpResponse = await Auth.signUp({
+        username: email,
+        password,
+        attributes: {
+          name,
+        },
+      });
 
-      // Sign in using AWS Amplify Auth
-      await Auth.signIn(email, password);
-      // Handle successful login (e.g., navigate to the next screen)
-      navigation.navigate('BasicDetail');
-    } catch (error) {
-      // Handle specific error types
-      if (error.code === 'UserNotConfirmedException') {
-        setError('Account not confirmed. Please check your email for a confirmation link.');
-      } else if (error.code === 'NotAuthorizedException') {
-        setError('Invalid email or password. Please try again.');
+      if (signUpResponse.userConfirmed === false) {
+        // Handle the case where email confirmation is required
+        setRegistrationResponse(
+          'A confirmation code has been sent to your email. Please check your inbox.'
+        );
       } else {
-        setError('An error occurred during login.');
+        // Handle successful registration
+        console.log('Registration successful', signUpResponse);
+        navigation.navigate('BasicDetail');
       }
-    } finally {
-      setIsLoading(false);
+    } catch (error) {
+      console.error('Error:', error);
+      setRegistrationResponse('An error occurred during registration');
     }
   };
 
@@ -94,6 +120,12 @@ const Login = ({ navigation }) => {
       source={require('../../assets/images/Landingbg.jpg')}
       style={styles.backgroundImage}>
       <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => console.log('')}></TouchableOpacity>
+          <Pressable onPress={() => navigation.navigate('BasicDetail')}>
+            <Text style={styles.backButton}>skip</Text>
+          </Pressable>
+        </View>
         <View style={styles.row}>
           <Image
             source={require('../../assets/images/C.png')}
@@ -105,29 +137,43 @@ const Login = ({ navigation }) => {
           />
         </View>
         <View style={styles.options}>
-          <TouchableOpacity onPress={() => console.log(' ')}>
-            <Text style={styles.optionText2}>Login</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+            <Text style={styles.optionText}>Login</Text>
           </TouchableOpacity>
           <View style={styles.separator} />
-          <TouchableOpacity onPress={() => navigation.navigate('RegisterAndSignup')}>
-            <Text style={styles.optionText}>Signup</Text>
+          <TouchableOpacity onPress={() => console.log(' ')}>
+            <Text style={styles.optionText2}>Signup</Text>
           </TouchableOpacity>
         </View>
-        <Text style={styles.welcomeText}>Hey there, </Text>
-        <Text style={styles.welcomeText2}>Welcome Back!</Text>
+        <Text style={styles.welcomeText}>Hey there,</Text>
+        <Text style={styles.welcomeText2}>Register Yourself!</Text>
 
-        {/* Email */}
+        {/* Name */}
         <View style={styles.container2}>
           <Animated.View
-            style={[styles.label, { transform: [{ translateY: transY.current }, { translateX: transX }] }]}>
-            <TouchableWithoutFeedback onPress={handleKeyboardDismiss}>
-              <Text style={styles.holder}>Email</Text>
-            </TouchableWithoutFeedback>
+            style={[styles.label, { transform: [{ translateY: transYN.current }, { translateX: transXN }] }]}>
+            <Text style={styles.holder}>Name</Text>
           </Animated.View>
           <TextInput
             style={styles.input}
-            onFocus={() => handleFocus(transY.current)}
-            onBlur={() => handleBlur(transY.current, email)}
+            onFocus={() => handleFocus(transYN.current)}
+            onBlur={() => handleBlur(transYN.current, name)}
+            placeholderTextColor="#000000"
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
+        </View>
+
+        {/* Email */}
+        <View style={styles.container3}>
+          <Animated.View
+            style={[styles.label, { transform: [{ translateY: transYE.current }, { translateX: transXE }] }]}>
+            <Text style={styles.holder}>Email</Text>
+          </Animated.View>
+          <TextInput
+            style={styles.input}
+            onFocus={() => handleFocus(transYE.current)}
+            onBlur={() => handleBlur(transYE.current, email)}
             placeholderTextColor="#000000"
             value={email}
             onChangeText={(text) => setEmail(text)}
@@ -135,12 +181,10 @@ const Login = ({ navigation }) => {
         </View>
 
         {/* Password */}
-        <View style={styles.containerP}>
+        <View style={styles.container3}>
           <Animated.View
             style={[styles.label, { transform: [{ translateY: transYP.current }, { translateX: transXP }] }]}>
-            <TouchableWithoutFeedback onPress={handleKeyboardDismiss}>
-              <Text style={styles.holder}>Password</Text>
-            </TouchableWithoutFeedback>
+            <Text style={styles.holder}>Password</Text>
           </Animated.View>
           <TextInput
             style={styles.input}
@@ -153,23 +197,36 @@ const Login = ({ navigation }) => {
           />
         </View>
 
-        <View style={styles.checkboxContainer}>
-          <CheckBox />
-          <Text style={styles.remember}>Remember me</Text>
-          <TouchableOpacity onPress={() => console.log('Forgot password pressed')}>
-            <Text style={styles.forgotPassword}>Forgot password?</Text>
-          </TouchableOpacity>
+        {/* Confirm Password */}
+        <View style={styles.container3}>
+          <Animated.View
+            style={[styles.label, { transform: [{ translateY: transYC.current }, { translateX: transXC }] }]}>
+            <Text style={styles.holder}>Confirm Password</Text>
+          </Animated.View>
+          <TextInput
+            style={styles.input}
+            onFocus={() => handleFocus(transYC.current)}
+            onBlur={() => handleBlur(transYC.current, confirmPassword)}
+            placeholderTextColor="#000000"
+            value={confirmPassword}
+            secureTextEntry={passwordInputType === 'password'}
+            onChangeText={(text) => setConfirmPassword(text)}
+          />
         </View>
-        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+
+        <TouchableOpacity style={styles.loginButton} onPress={handleRegister}>
           <View style={styles.row}>
             <Image source={require('../../assets/images/next.png')} />
-            <Text style={styles.loginButtonText}>Login</Text>
+            <Text style={styles.loginButtonText}>Signup</Text>
           </View>
         </TouchableOpacity>
-        {/* Display login response */}
-        {error && <Text style={styles.loginResponse}>{error}</Text>}
-        <TouchableOpacity onPress={() => navigation.navigate('RegisterAndSignup')}>
-          <Text style={styles.createAccount}>Don't have an account? Create one</Text>
+
+        {registrationResponse && (
+          <Text style={styles.registrationResponse}>{registrationResponse}</Text>
+        )}
+
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text style={styles.createAccount}>Already have an account? Login</Text>
         </TouchableOpacity>
       </View>
     </ImageBackground>
@@ -178,38 +235,45 @@ const Login = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 100,
+    paddingTop: 70,
     padding: 20,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  container2: {
+    borderRadius: 10,
+    width: '100%',
+  },
+  container3: {
+    borderRadius: 10,
+    width: '100%',
+    marginTop: 8,
   },
   label: {
     position: 'absolute',
     padding: 10,
   },
   holder: {
-    color: '#000000'
+    color: '#000000',
   },
-  container2: {
-    borderRadius: 10,
-    width: '100%'
-  },
-  containerP: {
-    borderRadius: 10,
-    width: '100%',
-    marginTop: 10
-  },
-  row: {
+  header: {
     flexDirection: 'row',
-    alignItems: 'center',
+    justifyContent: 'space-between',
+    alignSelf: 'stretch',
+  },
+  backButton: {
+    color: '#555555',
+    fontSize: 14,
+    fontFamily: 'open-sans-regular',
+    fontWeight: '400',
   },
   backgroundImage: {
     flex: 1,
     resizeMode: 'cover',
-  },
-  remember: {
-    color: '#000000',
-    paddingRight: 80
   },
   options: {
     flexDirection: 'row',
@@ -221,7 +285,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Poppins',
     fontWeight: '400',
     marginRight: 60,
-    marginLeft: 58
+    marginLeft: 58,
   },
   optionText2: {
     color: '#B51E71',
@@ -229,11 +293,11 @@ const styles = StyleSheet.create({
     fontFamily: 'Poppins',
     fontWeight: '400',
     marginRight: 60,
-    marginLeft: 58
+    marginLeft: 58,
   },
   separator: {
     width: 1,
-    backgroundColor: '#7B672',
+    backgroundColor: '#7B6F72',
     marginVertical: 5,
   },
   skipButton: {
@@ -247,20 +311,20 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
   },
   logo2: {
-    marginLeft: 3
+    marginLeft: 3,
   },
   welcomeText: {
     fontSize: 18,
     color: '#7B6F72',
     fontWeight: '400',
-    fontFamily: 'Poppins'
+    fontFamily: 'Poppins',
   },
   welcomeText2: {
     fontSize: 26,
     color: '#000000',
     paddingBottom: 20,
     fontWeight: 'bold',
-    fontFamily: 'Poppins'
+    fontFamily: 'Poppins',
   },
   input: {
     width: '100%',
@@ -268,8 +332,8 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 1,
     paddingLeft: 10,
-    marginBottom: 10,
-    color: '#000000'
+    marginBottom: 18,
+    color: '#000000',
   },
   checkboxContainer: {
     flexDirection: 'row',
@@ -284,7 +348,7 @@ const styles = StyleSheet.create({
   loginButton: {
     backgroundColor: '#B51E71',
     padding: 10,
-    marginTop: 70,
+    marginTop: 15,
     width: '100%',
     alignItems: 'center',
     borderRadius: 5,
@@ -295,16 +359,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontFamily: 'Poppins',
     fontSize: 16,
-    marginLeft: 10
+    marginLeft: 10,
   },
-  loginResponse: {
+  registrationResponse: {
     marginTop: 10,
-    color: 'red',
+    color: 'green',
   },
   createAccount: {
     color: 'blue',
-    marginTop: 20,
   },
 });
 
-export default Login;
+export default RegisterAndSignup;
