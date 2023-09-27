5a8fb207bb2187f3173b9e1e34214c19166ca8c8
sakshi3326 <saqkshiiitian123@gmail.com>
2023-09-27 20:29:02 +0530

    auth

--- razecued/index.js
+++ razecued/index.js
@@ -6,8 +6,8 @@ import {AppRegistry} from 'react-native';
 import Main from './src/screens/Main';
 import {name as appName} from './app.json';
 import { LogBox } from 'react-native';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
+import {Amplify} from 'aws-amplify';
+import config from './aws-export.js';
 
 LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
 Amplify.configure(config);
