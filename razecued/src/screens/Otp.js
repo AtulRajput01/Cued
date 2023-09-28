8203e21c2acacea3b9ec580768d086ce0b922897
Atul Rajput <92659293+AtulRajput01@users.noreply.github.com>
2023-09-28 16:15:13 +0530

    Update Otp.js

--- razecued/src/screens/Otp.js
+++ razecued/src/screens/Otp.js
@@ -5,7 +5,7 @@ import { Auth } from 'aws-amplify';
 const OTPVerification = ({ navigation, route }) => {
   const { email } = route.params;
   const [otp, setOTP] = useState('');
-  const [error, setError] = useState('');
+  const [verificationResponse, setVerificationResponse] = useState(null);
 
   const handleVerifyOTP = async () => {
     try {
