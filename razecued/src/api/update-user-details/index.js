36eb7eb86d3ab5e1c502b97aa03c12cea7fc6f03
Atul Rajput <92659293+AtulRajput01@users.noreply.github.com>
2023-09-30 17:01:06 +0530

    Update index.js

--- razecued/src/api/update-user-details/index.js
+++ razecued/src/api/update-user-details/index.js
@@ -1,16 +1,15 @@
 const AWS = require('aws-sdk');
-const { v4: uuidv4 } = require('uuid');
 
 AWS.config.update({
   region: 'us-east-1',
 });
 
-const dynamoDBTableName = 'users';
+const dynamoDBTableName = 'Users';
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
   try {
-    const { httpMethod, path } = event;
+    const { httpMethod, path, requestContext } = event;
 
     if (httpMethod === 'POST' && path === '/api/update-user-details') {
       // Extract request parameters from the request body
@@ -24,12 +23,15 @@ exports.handler = async (event) => {
         };
       }
 
+      // Use Cognito user ID as the unique identifier
+      const userId = requestContext.authorizer.claims.sub;
+
       // Save user's basic details to DynamoDB
-      const userId = await saveBasicDetails(collegeName, collegeId, passingYear, aadharCard);
+      await saveBasicDetails(userId, collegeName, collegeId, passingYear, aadharCard);
 
       return {
         statusCode: 200,
-        body: JSON.stringify({ success: true, message: 'Basic details submitted successfully', userId }),
+        body: JSON.stringify({ success: true, message: 'Basic details updated successfully', userId }),
       };
     } else {
       // Handle unknown or unsupported API endpoints
@@ -51,13 +53,10 @@ exports.handler = async (event) => {
 };
 
 // Function to save user's basic details to DynamoDB
-const saveBasicDetails = async (collegeName, collegeId, passingYear, aadharCard) => {
-  // Generate a unique user ID using UUID
-  const userId = uuidv4();
-
+const saveBasicDetails = async (userId, collegeName, collegeId, passingYear, aadharCard) => {
   // Prepare the item for DynamoDB
   const params = {
-    TableName: dynamoDBTableName,
+    TableName: 'Users',
     Item: {
       userId,
       collegeName,
@@ -70,9 +69,8 @@ const saveBasicDetails = async (collegeName, collegeId, passingYear, aadharCard)
   // Save the user details to DynamoDB
   try {
     await dynamodb.put(params).promise();
-    return userId;
   } catch (error) {
     console.error('Error saving user details:', error);
-    throw error; 
+    throw error;
   }
 };
