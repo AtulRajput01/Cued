b82b460f82d754c24551540ae211281076db4fc9
sakshi3326 <saqkshiiitian123@gmail.com>
2023-10-07 18:34:33 +0530

    register, login done

--- razecued/src/screens/Main.js
+++ razecued/src/screens/Main.js
@@ -20,57 +20,56 @@ import { Auth,Hub } from "aws-amplify";
 const Stack = createStackNavigator();
 
 export default function App() {
-  const [user, setUser] = useState(undefined);
-  const [loading, setLoading] = useState(true);
-  const checkUser = async () =>{
-    try{
-      const authUser = await Auth.currentAuthenticatedUser({bypassCache: true});
-      setUser(authUser);
-    }catch (e) {
-      setUser(null);
-    }finally {
-      setLoading(false);
-    }
+  // const [user, setUser] = useState(undefined);
+  // const [loading, setLoading] = useState(true);
+  // const checkUser = async () =>{
+  //   try{
+  //     const authUser = await Auth.currentAuthenticatedUser({bypassCache: true});
+  //     setUser(authUser);
+  //   }catch (e) {
+  //     setUser(null);
+  //   }finally {
+  //     setLoading(false);
+  //   }
    
-  };
+  // };
 
-  useEffect (() => {
-    checkUser();
-  }, []);
+  // useEffect (() => {
+  //   checkUser();
+  // }, []);
 
-  useEffect(() => {
-    const listener = data => {
-     if (data.payload.event == 'signIn'|| data.payload.event === 'signOut') {
-      checkUser();
-     }
-    };
+  // useEffect(() => {
+  //   const listener = data => {
+  //    if (data.payload.event == 'signIn'|| data.payload.event === 'signOut') {
+  //     checkUser();
+  //    }
+  //   };
 
-    Hub.listen('auth' , listener);
+  //   Hub.listen('auth' , listener);
 
-    return () => {
-      Hub.remove('auth', listener); // Remove the listener when the component unmounts
-    };
-  }, []);
+  //   return () => {
+  //     Hub.remove('auth', listener); // Remove the listener when the component unmounts
+  //   };
+  // }, []);
 
-  if (loading) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator />
-      </View>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  //       <ActivityIndicator />
+  //     </View>
+  //   );
+  // }
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash" headerMode="none">
-        {user ?(
+        
           <Stack.Screen name="Discover" component={Discover} />
-        ) :(
-          <>
+        
+         
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Register" component={Register} />
           <Stack.Screen name="Otp" component={Otp} />
-          </>
-        )}
+          
         <Stack.Screen name="Splash" component={Splash} />
         <Stack.Screen name="BasicDetail" component={BasicDetail} />
         
