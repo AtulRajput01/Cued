d74e0d494a050452b6d3522ee3e41667cabeed63
Atul Rajput <92659293+AtulRajput01@users.noreply.github.com>
2023-10-10 23:48:41 +0530

    Update layout.js

--- razecued/src/screens/layout.js
+++ razecued/src/screens/layout.js
@@ -9,43 +9,49 @@ Amplify.configure(awsconfig);
 
 export default function RootLayout() {
   useEffect(() => {
-   const removeListener = Hub.listen('auth', async (data) => { 
-    if (data.payload.event =='signIn') {
-      const userInfo = data.payload.data.attributes;
-      console.log(JSON.stringify(userInfo, null, 2));
+    const removeListener = Hub.listen('auth', async (data) => {
+      if (data.payload.event === 'signIn') {
+        const userInfo = data.payload.data.attributes;
+        console.log(JSON.stringify(userInfo, null, 2));
 
-      //DataStore.save(new user({ id: userInfo.sub, name}));
-      // save user to databse
-      const newUser = {
-        id: userInfo.sub,
-        name: userInfo.name,
-        email: userInfo.email,
-        user_collid: userInfo.user_collid,
-        user_collName: userInfo.user_collName,
-        user_passYear: userInfo.user_passYear,
-        user_age: userInfo.user_age,
-        user_gender: userInfo.user_gender,
-        user_phone: userInfo.user_phone,
-        user_altPhone: userInfo.user_altPhone,
-      };
-      await API.graphql({
-        query: Createusermutation,
-        variables: { input: newUser },
-      });        
-    }
-  });
+        // Check if the user already exists in the database
+        const existingUser = await DataStore.query(User, userInfo.sub);
 
-  return () => {
-    removeListner();
-   };
+        //DataStore.save(new user({ id: userInfo.sub, name}));
+
+        if (!existingUser) {
+          // If the user doesn't exist, save user to the database
+          const newUser = {
+            id: userInfo.sub,
+            name: userInfo.name,
+            email: userInfo.email,
+            user_collid: userInfo.user_collid,
+            user_collName: userInfo.user_collName,
+            user_passYear: userInfo.user_passYear,
+            user_age: userInfo.user_age,
+            user_gender: userInfo.user_gender,
+            user_phone: userInfo.user_phone,
+            user_altPhone: userInfo.user_altPhone,
+          };
+
+          await API.graphql({
+            query: Createusermutation,
+            variables: { input: newUser },
+          });
+        }
+      }
+    });
+
+    return () => {
+      removeListener();
+    };
   }, []);
 
- return (
-   <Authenticator.Provider>
-     <Authenticator>
-       <Stack screenOptions={{ headerShown: false }) />
-     </Authenticator>
-   </Authenticator.Provider>
- );
+  return (
+    <Authenticator.Provider>
+      <Authenticator>
+        <Stack screenOptions={{ headerShown: false }} />
+      </Authenticator>
+    </Authenticator.Provider>
+  );
 }
-
