be04599683e99bb0ca920ff7ec9da43731bc52aa
Atul Rajput <92659293+AtulRajput01@users.noreply.github.com>
2023-10-10 23:37:55 +0530

    Update layout.js

--- razecued/src/screens/layout.js
+++ razecued/src/screens/layout.js
@@ -9,12 +9,29 @@ Amplify.configure(awsconfig);
 
 export default function RootLayout() {
   useEffect(() => {
-    if (data.payload.event =='signin') {
+   const removeListener = Hub.listen('auth', async (data) => { 
+    if (data.payload.event =='signIn') {
       const userInfo = data.payload.data.attributes;
       console.log(JSON.stringify(userInfo, null, 2));
 
-      DataStore.save(new user({ id: userInfo.sub, name}));
-      
+      //DataStore.save(new user({ id: userInfo.sub, name}));
+      // save user to databse
+      const newUser = {
+        id: userInfo.sub,
+        name: userInfo.name,
+        email: userInfo.email,
+        user_collid: userInfo.user_collid,
+        user_collName: userInfo.user_collName,
+        user_passYear: userInfo.user_passYear,
+        user_age: userInfo.user_age,
+        user_gender: userInfo.user_gender,
+        user_phone: userInfo.user_phone,
+        user_altPhone: userInfo.user_altPhone,
+      };
+      await API.graphql({
+        query: Createusermutation,
+        variables: { input: newUser },
+      });        
     }
   });
 
