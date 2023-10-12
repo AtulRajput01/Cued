a5607b81a649d68f6c1a12b9e13a50ab2cfc803a
AtulRajput01 <atul2020csai110@abesit.edu.in>
2023-10-12 11:03:25 +0530

    implement a persistent authentication

--- razecued/index.js
+++ razecued/index.js
@@ -2,18 +2,20 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import Main from './src/screens/Main';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import { LogBox } from 'react-native';
-import {Amplify} from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import config from './aws-export.js';
 import 'core-js/full/symbol/async-iterator';
 import 'expo-router/entry';
-import {withAutheticator} from 'aws-amplify-react-native';
+import { withAuthenticator } from 'aws-amplify-react-native';
 
 LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
 Amplify.configure(config);
-AppRegistry.registerComponent(appName, () => Main);
 
+const MainWithAuth = withAuthenticator(Main);
+
+AppRegistry.registerComponent(appName, () => MainWithAuth);
 
