6b4d2124f239d259dd8c84fb8f7fe59198640051
Atul Rajput <92659293+AtulRajput01@users.noreply.github.com>
2023-10-12 11:12:58 +0530

    Update auth

--- razecued/index.js
+++ razecued/index.js
@@ -8,9 +8,6 @@ import { name as appName } from './app.json';
 import { LogBox } from 'react-native';
 import { Amplify } from 'aws-amplify';
 import config from './aws-export.js';
-import 'core-js/full/symbol/async-iterator';
-import 'expo-router/entry';
-import { withAuthenticator } from 'aws-amplify-react-native';
 
 LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
 Amplify.configure(config);
