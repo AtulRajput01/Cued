02766ad1fcb963d2ed58309515b285e54ed24b20
Atul Rajput <92659293+AtulRajput01@users.noreply.github.com>
2023-10-19 19:33:01 +0530

    Update index.js

--- razecued/src/api/register-for-event/index.js
+++ razecued/src/api/register-for-event/index.js
@@ -1,135 +1,58 @@
 const AWS = require('aws-sdk');
-
-AWS.config.update({
-  region: 'us-east-1',
-});
-
-const usersTableName = 'Users';
-const eventsTableName = 'Events';
-const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-  let response;
-  console.log(event);
-  switch (event.httpMethod) {
-    case 'POST':
-      if (event.path === '/api/register-event') {
-        response = await registerEvent(JSON.parse(event.body));
-      } else {
-        response = await saveUser(JSON.parse(event.body));
-      }
-      break;
-    case 'GET':
-      if (event.path === '/api/fetch-registered-events') {
-        response = await fetchRegisteredEvents();
-      } else {
-        response = await getUsers();
-      }
-      break;
-    case 'PUT':
-      const requestBody = JSON.parse(event.body);
-      response = await updateUser(requestBody.id, requestBody.updateKey, requestBody.updateValue);
-      break;
-    case 'DELETE':
-      response = await deleteUser(JSON.parse(event.body).id);
-      break;
-    default:
-      response = buildResponse(404, '404 not found');
+  try {
+    const { id: userId, id: eventId } = JSON.parse(event.body);
+
+    // Fetch event details to ensure it exists
+    const eventDetails = await getEventDetails(eventId);
+
+    if (!eventDetails) {
+      return buildResponse(404, 'Event not found');
+    }
+
+    // Update user's record in the Users DynamoDB table
+    await updateUserEvents(userId, eventId);
+
+    return buildResponse(200, 'User registered for the event successfully');
+  } catch (error) {
+    console.error('Error registering user for event:', error);
+    return buildResponse(500, 'Internal server error');
   }
-  return response;
 };
 
-async function saveUser(requestBody) {
+async function getEventDetails(eventId) {
   const params = {
-    TableName: usersTableName,
-    Item: requestBody,
-  };
-  return await dynamodb
-    .put(params)
-    .promise()
-    .then(() => {
-      const body = {
-        Operation: 'SAVE',
-        Message: 'SUCCESS',
-        Item: requestBody,
-      };
-      return buildResponse(200, body);
-    })
-    .catch((error) => {
-      console.error(error);
-      return buildResponse(500, 'Internal Server Error');
-    });
-}
-
-async function registerEvent(requestBody) {
-  const params = {
-    TableName: eventsTableName,
-    Item: requestBody,
+    TableName: 'Events-uvz42cvcjncbnoq7sctsiqiqxy-dev',
+    Key: { id: eventId },
   };
 
-  return await dynamodb
-    .put(params)
-    .promise()
-    .then(() => {
-      const body = {
-        success: true,
-        message: 'Event registered successfully',
-        data: requestBody,
-      };
-      return buildResponse(200, body);
-    })
-    .catch((error) => {
-      console.error(error);
-      return buildResponse(500, 'Internal Server Error');
-    });
+  const result = await dynamoDB.get(params).promise();
+  return result.Item;
 }
 
-async function fetchRegisteredEvents() {
-  // Logic to fetch registered events
-  // Replace with your actual logic to fetch events from the 'Events' table
-  const eventData = [
-    {
-      eventId: '1',
-      eventPoster: 'https.//poster.jpg',
-      eventName: 'Music Festival',
-      eventDescription: 'Join us for a night of music and fun.',
+async function updateUserEvents(userId, eventId) {
+  const userParams = {
+    TableName: 'Users-uvz42cvcjncbnoq7sctsiqiqxy-dev',
+    Key: { id: userId },
+    UpdateExpression: 'SET #events = list_append(if_not_exists(#events, :emptyList), :event)',
+    ExpressionAttributeNames: { '#events': 'events' },
+    ExpressionAttributeValues: {
+      ':emptyList': [],
+      ':event': [eventId],
     },
-    {
-      eventId: '2',
-      eventPoster: 'https.//poster.jpg',
-      eventName: 'Art Exhibition',
-      eventDescription: 'Explore beautiful artworks from local artists.',
-    },
-  ];
-
-  const body = {
-    success: true,
-    message: 'Registered events fetched successfully',
-    data: eventData,
   };
 
-  return buildResponse(200, body);
+  await dynamoDB.update(userParams).promise();
 }
 
-async function getUsers() {
-  const params = {
-    TableName: 'Users',
-  };
-  const allUsers = await dynamodb.scan(params).promise();
-  const body = {
-    users: allUsers,
-  };
-  return buildResponse(200, body);
-}
-
-// Other existing functions...
-
-function buildResponse(statusCode, body) {
+function buildResponse(statusCode, message) {
   return {
-    statusCode: statusCode,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
+    statusCode,
+    body: JSON.stringify({
+      success: statusCode === 200,
+      message,
+    }),
   };
 }
