f7eca878dfbf7d7d517a9583ca4bdbdf92b05e9d
AtulRajput01 <atul2020csai110@abesit.edu.in>
2023-11-08 21:05:15 +0530

    add sentry

--- razecued/src/screens/Userdetail.js
+++ razecued/src/screens/Userdetail.js
@@ -1,47 +1,17 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
-import { View, Text, StyleSheet, Image, Pressable, TouchableOpacity } from 'react-native';
+import React, { useState, useCallback, useRef } from 'react';
+import { View, Text, StyleSheet,Image, Pressable, TouchableOpacity,  TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
 import ImagePicker from 'react-native-image-crop-picker';
 import ActionSheet from 'react-native-actionsheet';
 import { ImageBackground } from 'react-native';
-import { Auth } from 'aws-amplify'; // Import the Auth module from Amplify
 
 const UserDetail = () => {
   const navigation = useNavigation();
   const [profilePicture, setProfilePicture] = useState(null);
-  const [userDetails, setUserDetails] = useState(null);
   const actionSheetRef = useRef(null);
 
-  useEffect(() => {
-    // Retrieve the current authenticated user's information
-    const fetchUserDetails = async () => {
-      try {
-        const user = await Auth.currentAuthenticatedUser();
-        const userId = user.attributes.sub; // Use the Cognito ID as the unique identifier
-        
-        console.log('Cognito ID:', userId);
-
-        // Fetch user details from the API endpoint using the Cognito ID
-        const response = await fetch(`https://hk1630uulc.execute-api.us-east-1.amazonaws.com/Dev/profile/${userId}`);
-        const data = await response.json();
-
-        console.log('API Response:', data);
-
-        if (data.success && data.data.user) {
-          setUserDetails(data.data.user);
-          console.log('User Details Set:', data.data.user);
-        } else {
-          console.error('API Error:', data.error);
-        }
-      } catch (error) {
-        console.error('Error fetching user details:', error);
-      }
-    };
-
-    fetchUserDetails();
-  }, []);
-
+  
   const handleImagePicker = useCallback(() => {
     actionSheetRef.current.show();
   }, []);
@@ -52,13 +22,11 @@ const UserDetail = () => {
       height: 300,
       cropping: true,
       mediaType: 'photo',
-    })
-      .then((image) => {
-        setProfilePicture(image.path);
-      })
-      .catch((error) => {
-        console.log('CameraPicker error:', error);
-      });
+    }).then(image => {
+      setProfilePicture(image.path);
+    }).catch(error => {
+      console.log('CameraPicker error:', error);
+    });
   };
 
   const handleImageFromDevice = () => {
@@ -67,13 +35,11 @@ const UserDetail = () => {
       height: 300,
       cropping: true,
       mediaType: 'photo',
-    })
-      .then((image) => {
-        setProfilePicture(image.path);
-      })
-      .catch((error) => {
-        console.log('ImagePicker error:', error);
-      });
+    }).then(image => {
+      setProfilePicture(image.path);
+    }).catch(error => {
+      console.log('ImagePicker error:', error);
+    });
   };
 
   const removePhoto = () => {
@@ -81,61 +47,116 @@ const UserDetail = () => {
   };
 
   return (
-    <ImageBackground
-      source={require('../../assets/images/Profilebg.jpg')}
-      style={styles.backgroundImage}
-    >
-      <View style={styles.container}>
-        <View style={styles.row}>
-          <Pressable onPress={() => navigation.navigate('Home')}>
-            <View style={styles.text}>
-              <Image source={require('../../assets/images/arrow_left.png')} style={styles.arrowImage} />
-            </View>
-          </Pressable>
-          <Text style={styles.text1}>User Details</Text>
-        </View>
-        <TouchableOpacity style={styles.circleContainer} onPress={profilePicture ? handleImagePicker : null}>
-          {profilePicture ? (
-            <>
-              <Image source={{ uri: profilePicture }} style={styles.profilePicture} />
-              <TouchableOpacity style={styles.removePhotoButton} onPress={removePhoto}>
-                {/* Remove Photo Button */}
-              </TouchableOpacity>
-            </>
-          ) : (
-            <Icon name="camera" size={30} onPress={handleImagePicker} />
-          )}
-        </TouchableOpacity>
-        <ActionSheet
-          ref={actionSheetRef}
-          options={profilePicture ? ['Remove Photo', 'Cancel'] : ['Camera', 'From Device', 'Cancel']}
-          cancelButtonIndex={profilePicture ? 1 : 2}
-          onPress={(index) => {
-            if (index === 0 && profilePicture) {
-              removePhoto();
-            } else if (index === 0) {
-              handleCameraPicker();
-            } else if (index === 1) {
-              handleImageFromDevice();
-            }
-          }}
-        />
 
-        {userDetails && (
-          <View>
-            <View style={styles.textRow}>
-              <Text style={styles.label}>Name:</Text>
-              <Text style={styles.value}>{userDetails.name}</Text>
-            </View>
-            <View style={styles.textRow}>
-              <Text style={styles.label}>Age:</Text>
-              <Text style={styles.value}>{userDetails.age}</Text>
-            </View>
-            {/* Add more views for other user details */}
+    <ImageBackground
+    source={require('../../assets/images/Profilebg.jpg')} 
+      style={styles.backgroundImage}>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Pressable onPress={() => navigation.navigate('Home')}>
+          <View style={styles.text}>
+          <Image source={require('../../assets/images/arrow_left.png')} style={styles.arrowImage}  />
           </View>
-        )}
+        </Pressable>
+        <Text style={styles.text1}>User Details</Text>
       </View>
+      <TouchableOpacity style={styles.circleContainer} onPress={profilePicture ? handleImagePicker : null}>
+        {profilePicture ? (
+          <>
+            <Image source={{ uri: profilePicture }} style={styles.profilePicture} />
+            <TouchableOpacity style={styles.removePhotoButton} onPress={removePhoto}>
+              {/* Remove Photo Button */}
+            </TouchableOpacity>
+          </>
+        ) : (
+          <Icon name="camera" size={30} onPress={handleImagePicker} />
+        )}
+      </TouchableOpacity>
+      <ActionSheet
+        ref={actionSheetRef}
+        options={profilePicture ? ['Remove Photo', 'Cancel'] : ['Camera', 'From Device', 'Cancel']}
+        cancelButtonIndex={profilePicture ? 1 : 2}
+        onPress={(index) => {
+          if (index === 0 && profilePicture) {
+            removePhoto();
+          } else if (index === 0) {
+            handleCameraPicker();
+          } else if (index === 1) {
+            handleImageFromDevice();
+          }
+        }}
+      />
+
+      <TextInput
+        style={styles.input1}
+        placeholder="Name"
+        placeholderTextColor="#A9A9A9"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Age"
+        keyboardType="numeric"
+        placeholderTextColor="#A9A9A9"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Gender"
+        placeholderTextColor="#A9A9A9"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="DOB (Date of Birth)"
+        keyboardType="numeric"
+        placeholderTextColor="#A9A9A9"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        placeholderTextColor="#A9A9A9"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        placeholderTextColor="#A9A9A9"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contact Number"
+        keyboardType="numeric"
+        placeholderTextColor="#A9A9A9"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Alternate Number"
+        keyboardType="numeric"
+        placeholderTextColor="#A9A9A9"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="College Name"
+        keyboardType="numeric"
+        placeholderTextColor="#A9A9A9"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Passing year"
+        keyboardType="numeric"
+        placeholderTextColor="#A9A9A9"
+      />
+     
+
+<View style={styles.gap} />
+      <TouchableOpacity style={styles.button}>
+        <View style={styles.row}>
+                <Image source={require('../../assets/images/check.png')}/>
+                <Text style={styles.buttonText}>Save changes</Text>
+        </View>
+              </TouchableOpacity>
+    </View>
     </ImageBackground>
+
   );
 };
 
@@ -146,19 +167,50 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
   },
-  label: {
-    color: '#000000',
-  },
-  value: {
-    color: '#000000',
-  },
   backgroundImage: {
     flex: 1,
-    resizeMode: 'cover',
+    resizeMode: 'cover'
+  },
+  gap: {
+    height: 20,
   },
   arrowImage: {
     width: 20,
     height: 20,
+  },
+  button: {
+    
+    backgroundColor: '#B51E71',
+    paddingVertical: 8,
+    borderRadius: 3,
+    width: '100%',
+    alignSelf: 'center',
+    alignItems: 'center',
+    borderRadius: 5
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+    fontFamily: 'Poppins',
+    marginLeft:10
+   
+  },
+  removePhotoButton: {
+    position: 'absolute',
+    bottom: 5,
+    right: 5,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    padding: 5,
+    borderRadius: 5,
+  },
+  removePhotoText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   text1: {
     padding: 10,
@@ -169,6 +221,10 @@ const styles = StyleSheet.create({
   },
   text: {
     padding: 10,
+  },
+  secondText: {
+    paddingLeft: 20,
+    fontSize: 18,
   },
   circleContainer: {
     width: 100,
@@ -186,25 +242,24 @@ const styles = StyleSheet.create({
     height: '100%',
     borderRadius: 50,
   },
-  removePhotoButton: {
-    position: 'absolute',
-    bottom: 5,
-    right: 5,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    padding: 5,
+  input1: {
+    width: '100%',
+    height: 40,
+    marginTop: 30,
+    borderWidth: 1,
+    borderColor: '#000000',
     borderRadius: 5,
+    paddingLeft: 10,
   },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  textRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+  input: {
+    width: '100%',
+    height: 40,
     marginTop: 10,
+    borderWidth: 1,
+    borderColor: '#000000',
+    borderRadius: 5,
+    paddingLeft: 10,
   },
 });
 
 export default UserDetail;
-
